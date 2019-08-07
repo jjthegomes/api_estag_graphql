65290ce653743846386faad422b3c2194407111a
Jonas Gomes <jj.ti@hotmail.com>
2019-08-06 23:03:13 -0300

    Refactoring #1

--- src/app/mutation/vaga/VagaMutation.js
+++
@@ -1,9 +0,0 @@
-const addVaga = require("./AddVaga").addVaga;
-const updateVaga = require("./UpdateVaga").updateVaga;
-const removeVaga = require("./DeleteVaga").removeVaga;
-
-module.exports = {
-  addVaga,
-  updateVaga,
-  removeVaga
-};
